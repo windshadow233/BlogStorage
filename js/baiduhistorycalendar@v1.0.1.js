a4d015c279f01b5cf5693c16bdf01627e84ee93f
WindShadow <50669888+windshadow233@users.noreply.github.com>
2024-11-11 23:24:34 +0800

    Update and rename baiduhistorycalendar.js to baiduhistorycalendar@v1.0.1.js

--- js/baiduhistorycalendar@v1.0.1.js
+++ js/baiduhistorycalendar@v1.0.1.js
@@ -30,7 +30,7 @@ if(document.getElementById('history-container')){
             getDate = String(getDate);
         }
         var getMonthDate = "S" + getMonth + getDate;
-        return ["https://cdn.jsdelivr.net/gh/Zfour/Butterfly-card-history@latest/baiduhistory/json/" + getMonth + ".json",getMonthDate]
+        return ["https://fastly.jsdelivr.net/gh/Zfour/Butterfly-card-history@latest/baiduhistory/json/" + getMonth + ".json",getMonthDate]
     }
     var history_data = history_get_data()
     fetch(history_data[0]).then(data=>data.json()).then(data=>{
